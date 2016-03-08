7dc8687f3b45442a75cc6e91ebcde1e23126c8b5
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-03-07 23:11:37 -0600

    learn scala week3 fies

---
+++ scala/src/week3/Hello.scala
@@ -0,0 +1,9 @@
+package week3
+
+
+object Hello {
+  def main(args:Array[String]) {
+    println("hello world")
+  }
+  def printFun = println("hellowwww")
+}
---
+++ scala/src/week3/Hello2.scala
@@ -0,0 +1,9 @@
+package week3
+
+import week3.Hello.printFun
+
+object Hello2 {
+  def main(args:Array[String]) = {
+    printFun
+  }
+}

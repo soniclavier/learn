e4f0c1b929cde91cc35a6c0d8907cc2ad5d0b773
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2017-02-09 01:18:35 -0500

    manning fn pgm

--- scala/src/main/scala/manning_functional_programming_.scala
+++ scala/src/main/scala/manning_functional_programming_.scala
@@ -123,3 +123,36 @@ def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = (as, b
 	case (_, Nil) => Nil
 	case (a::at, b::bt) => f(a,b)::zipWith(at, bt)(f)
 }
+
+def hasSubsequence[A](l: List[A], s: List[A]): Boolean = (l, s) match {
+	case (_, Nil) => true
+	case (Nil, _) => false
+	case (a::at, b::bt) => if (a == b) hasSubsequence(at, bt) else hasSubsequence(at, b::bt)
+}
+
+//trees
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def size[A](t: Tree[A]): Int = {
+	t match {
+		case Leaf(_) => 1
+		case Branch(l, r) => 1 + size(l) + size(r)
+	}
+}
+
+def maximum(t: Tree[Int]): Int = t match {
+	case Leaf(v) => v
+	case Branch(l, r) => maximum(l) max maximum(r)
+}
+
+def depth[A](t: Tree[A]): Int = t match {
+	case Leaf(_) => 0
+	case Branch(l, r) => 1 + (depth(l) max depth(r))
+}
+
+def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+	case Leaf(v) => Leaf(f(v))
+	case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+}

1b28f4275a334d061b769c0799cf8f5ef946475c
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2018-09-28 09:55:33 -0700

    arrays and pointers

---
+++ go/src/hello/arrays.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+	var matrix [2]int16
+
+	matrix[0] = 1
+	matrix[1] = 2
+
+	for i,v := range matrix {
+		matrix[i] = 0	//range creates a copy of the array, the value does not get udpated
+		println(v)
+	}
+
+
+	//row traversal is faster than column traversal
+	//predictable access pattern :  (how data is loaded into cpu)
+}
--- go/src/hello/pointers.go
+++ go/src/hello/pointers.go
@@ -1,5 +1,36 @@
-package com.vishnuviswanath
+package main
+
+//import "fmt"
+
+type rs struct {
+	id int
+}
+
+//increment the value of the pointer.
+func increment(inc *int) {
+	*inc++
+}
+
+func funWithPointerReturn() *rs {
+	r := rs {
+		id: 1,
+	}
+
+	return &r
+}
 
 func main() {
-	
+	i := 10
+	println("Pointer variable value before passing reference",i)
+	increment(&i) // pass the address
+	println("Pointer variable value after passing reference", i)
+
+	//fmt makes values escape to heap??
+
+	//the value escapes to heap, since the stack in which the value r was constrcuted does not exist(can be overwritten in the next call) after the function
+	//* so no need of garbage collection in stack, because the next call will initialize the memory to 'zero-value'. => having the value in stack is performant.
+	r := funWithPointerReturn()
+	println(r)
 }
+
+
